f991ab6985a509611009ee81eae0f0209b23e394
alex_tran <lxtran@gmail.com>
2019-06-26 23:28:13 -0700

    added feature to pass data from parent component

---
+++ src/components/subcomponents/Welcome.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Welcome(props) {
+  console.log(props.children); //Logs the <p> tag from Header
+  console.log(<hr />); //Logs a blank object
+  return (
+    <div>
+      <h1>Hello, {props.name}</h1>
+      {/* <p>{this.props.children}</p> */}
+      <hr />
+      {props.children}
+    </div>
+  );
+}
+
+export default Welcome;
